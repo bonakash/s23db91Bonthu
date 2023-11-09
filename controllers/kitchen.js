e940fb5590aa710174ab08626241d24e3897940a
bonakash <bonthuakash1@gmail.com>
2023-11-08 21:58:42 -0600

    changes made

--- controllers/kitchen.js
+++ controllers/kitchen.js
@@ -1,8 +1,15 @@
 var Kitchen = require('../models/kitchenSchema');
 // List of all Kitchens
-exports.kitchen_list = function(req, res) {
-res.send('NOT IMPLEMENTED: Kitchen list');
-};
+exports.kitchen_list = async function(req, res) {
+    try{
+    theItems = await Kitchen.find();
+    res.send(theItems);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
 // for a specific Kitchen.
 exports.kitchen_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Kitchen detail: ' + req.params.id);
@@ -19,3 +26,16 @@ res.send('NOT IMPLEMENTED: Kitchen delete DELETE ' + req.params.id);
 exports.kitchen_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: Kitchen update PUT' + req.params.id);
 };
+
+// VIEWS
+// Handle a show all view
+exports.kitchen_view_all_Page = async function(req, res) {
+    try{
+    thekitchen= await kitchen.find();
+    res.render('kitchen', { title: 'kitchen Search Results', results: thekitchen });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
