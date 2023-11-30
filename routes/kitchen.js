5f48324a591ce5ca8c060f7058b4cb3567abe950
bonakash <bonthuakash1@gmail.com>
2023-11-29 21:38:14 -0600

    Changes made with some added additions

--- routes/kitchen.js
+++ routes/kitchen.js
@@ -1,6 +1,14 @@
 var express = require('express');
 const kitchen_controlers= require('../controllers/kitchen');
 var router = express.Router();
+
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+}
+
 /* GET kitchens */
 router.get('/', kitchen_controlers.kitchen_view_all_Page );
 /* GET detail kitchen page */
@@ -8,7 +16,7 @@ router.get('/detail', kitchen_controlers.kitchen_view_one_Page);
 /* GET create kitchen page */
 router.get('/create', kitchen_controlers.kitchen_create_Page);
 /* GET create update page */
-router.get('/update', kitchen_controlers.kitchen_update_Page);
+router.get('/update',secured, kitchen_controlers.kitchen_update_Page);
 /* GET delete kitchen page */
 router.get('/delete', kitchen_controlers.kitchen_delete_Page);
 
